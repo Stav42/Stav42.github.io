5191dc5e7d5125f73bb40b0c26e6f77c47a6418c
Stav42 <vaditya@iitk.ac.in>
2023-12-08 10:33:10 +0530

    collapsible paragraphs added

--- rtscript.js
+++ rtscript.js
@@ -8,3 +8,29 @@ function countWords() {
 }
 
 document.getElementById("reading-time").innerText = "Reading Time: " + countWords();
+
+var coll = document.getElementsByClassName("pop_arrow");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var topic_name = this.id + "_content";
+        var content = document.getElementById(topic_name);
+        // if (content.style.display === "block") {
+        //     content.style.display = "none";
+        //     this.style.transform = "rotate(135deg)";
+        // } else {
+        //     content.style.display = "block";
+        //     this.style.transform = "rotate(315deg)";
+            
+        // }
+        console.log(content.style.maxHeight)
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+            this.style.transform = "rotate(135deg)";
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            this.style.transform = "rotate(315deg)";
+          }
+    });
+}
